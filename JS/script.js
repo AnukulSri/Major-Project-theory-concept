b6eac788834f962a8e0c3620fcd0e60b4d8c4f2f
Anukul Srivastava <anukulsrivastava123@gmail.com>
2023-04-18 20:11:39 +0530

    Update script.js

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+var a = document.getElementById('head')
+a.style.color = 'red'
+a.style.background = 'green'
+
+var b = document.getElementById('para')
+b.style.color = 'cyan'
+b.style.background = 'pink'
+
+var c = document.getElementsByClassName('main')
+c[0].style.color='purple'
+c[1].style.color = 'Orange'
+c[1].style.fontFamily = 'cursive'
+
+
+var d= document.getElementsByClassName('const')
+d[0].style.color = 'Orange'
+d[0].style.fontSize= '32px'
+
+d[1].style.color = 'Green'
+d[1].style.fontSize = '32px'
+
+d[2].style.color = 'Red'
+d[2].style.fontSize = '32px'
+
